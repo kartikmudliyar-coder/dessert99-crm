9858893bda4cd851988725f5c40495c745e36480
kartikmudliyar-coder <kightverse@gmail.com>
2025-09-22 18:43:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
+import { createClientBrowser } from "@/utils/supabase/client";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
     const checkUser = async () => {
-      const supabase = createClient();
+      const supabase = createClientBrowser();
       const { data } = await supabase.auth.getUser();
 
       if (data?.user) {
-        router.replace("/dashboard"); // logged in → dashboard
+        router.replace("/dashboard");
       } else {
-        router.replace("/login"); // not logged in → login
+        router.replace("/login");
       }
     };
 
