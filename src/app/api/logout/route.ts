07a808cae7effbcd07c790acb5398e99eb9f8d57
kartikmudliyar-coder <kightverse@gmail.com>
2025-09-23 18:07:16 +0530

    Update route.ts

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,11 +1,8 @@
-import { supabaseServer } from '@/lib/supabaseServer';
 import { NextResponse } from 'next/server';
+import { createSupabaseServerClient } from '@/lib/supabaseServer';
 
 export async function POST() {
-  const supabase = supabaseServer;
-
-  const { error } = await supabase.auth.signOut();
-
-  if (error) return NextResponse.json({ error: error.message }, { status: 400 });
-  return NextResponse.json({ message: 'Logged out successfully' });
+  const supabase = createSupabaseServerClient();
+  await supabase.auth.signOut();
+  return NextResponse.redirect('/login');
 }
