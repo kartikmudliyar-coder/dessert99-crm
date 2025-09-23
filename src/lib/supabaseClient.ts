0bcdf9c3ed5dd7790d6e382cbbf0bf964cc13f48
kartikmudliyar-coder <kightverse@gmail.com>
2025-09-23 16:08:32 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,10 @@
-// src/utils/supabase/client.ts
+"use client";
+
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClientBrowser = () => {
-  return createBrowserClient({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  });
-};
+export function createClientBrowser() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
