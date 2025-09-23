ef8f612abe32746200faebab0a380dd9be32fb14
kartikmudliyar-coder <kightverse@gmail.com>
2025-09-23 17:59:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+// next.config.js
+module.exports = {
+  experimental: { appDir: true },
 };
-module.exports = nextConfig;
